23a95096c2e2fb1c48d8402ad69df3445e09f7b4
Your Name <you@example.com>
2024-05-24 15:15:40 -0700

    HeySQuAD

--- eval/load_via_eval.py
+++ eval/load_via_eval.py
@@ -8,21 +8,43 @@ def load_via_eval(
     dataset_name,
 ):
     if dataset_name == "WillHeld/SD-QA":
-        # the name of x and y
-        x_label, y_label = "question", "answers"
-        # load the right partition
-        ds = load_dataset(dataset_name)["dev"]
-        # filter
-        ds = ds.filter(lambda example: example[y_label])
+        return load_SDQA(dataset_name)
     elif dataset_name == "SALT-NLP/speech_fairness":
-        # the name of x and y
-        x_label, y_label = "audio", "transcription"
-        # load the right partition
-        ds = load_dataset(dataset_name).train_test_split(
-            test_size=TEST_SIZE,
-            seed=SEED,
-        )
-        # filter
-        ds = ds.filter(lambda example: example[y_label])
+        return load_speech_fairness(dataset_name)
 
+
+def load_SDQA(dataset_name="WillHeld/SD-QA"):
+    # https://huggingface.co/datasets/WillHeld/SD-QA
+    # the name of x and y
+    x_label, y_label = "question", "answers"
+    # load the right partition
+    ds = load_dataset(dataset_name)["dev"]
+    # filter
+    ds = ds.filter(lambda example: example[y_label])
+
+    return x_label, y_label, ds
+
+
+def load_speech_fairness(dataset_name="SALT-NLP/speech_fairness"):
+    # https://huggingface.co/datasets/SALT-NLP/speech_fairness
+    # the name of x and y
+    x_label, y_label = "audio", "transcription"
+    # load the right partition
+    ds = load_dataset(dataset_name).train_test_split(
+        test_size=TEST_SIZE,
+        seed=SEED,
+    )
+    # filter
+    ds = ds.filter(lambda example: example[y_label])
+    return x_label, y_label, ds
+
+
+def load_HeySquad(dataset_name="yijingwu/HeySQuAD_human"):
+    # https://huggingface.co/datasets/yijingwu/HeySQuAD_human
+    # the name of x and y
+    x_label, y_label = "audio", "answers"
+    # load the right partition
+    ds = load_dataset(dataset_name)["validation"]
+    # filter
+    ds = ds.filter(lambda example: example[y_label])
     return x_label, y_label, ds
