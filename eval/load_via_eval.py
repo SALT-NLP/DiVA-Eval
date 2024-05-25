d483a31d8ab9e3e01e4ffe1ef998b8448513ef52
Your Name <you@example.com>
2024-05-24 21:33:57 -0700

    fleurs

--- eval/load_via_eval.py
+++ eval/load_via_eval.py
@@ -6,6 +6,7 @@ TEST_SIZE = 0.2  # if no val/test already
 
 def load_via_eval(
     dataset_name,
+    langauge,
 ):
     if dataset_name == "Spoken_Dialect_QA":
         return load_SDQA()
@@ -21,6 +22,10 @@ def load_via_eval(
         return load_MELD_emotion_recognition()
     elif dataset_name == "Mustard_sarcasm":
         return load_mustard_sarcasm()
+    elif dataset_name == "CommonVoice_speaker_identity":
+        return load_commonvoice_classification(language=langauge)
+    elif dataset_name == "FLEURS_speaker_identity":
+        return load_google_fleurs_speaker_identify(language=langauge)
 
 
 def load_SDQA(dataset_name="WillHeld/SD-QA"):
@@ -246,12 +251,13 @@ def load_mustard_sarcasm(dataset_name="SALT-NLP/Mustard_sarcasm"):
 
 
 def load_commonvoice_classification(
+    language,
     dataset_name="mozilla-foundation/common_voice_17_0",
 ):
     import numpy as np
 
     # https://huggingface.co/datasets/mozilla-foundation/common_voice_17_0
-    # features: ['client_id', 'path', 'audio', 'sentence', 'up_votes', 'down_votes', 'age', 'gender', 'accent', 'locale', 'segment', 'variant']
+    # features = ['client_id', 'path', 'audio', 'sentence', 'up_votes', 'down_votes', 'age', 'gender', 'accent', 'locale', 'segment', 'variant']
     # example_data = {
     #     "client_id": "000abb3006b78ea4c1144e55d9d158f05a9db0110160510fef2b006f2c2c8e35f7bb538b04542511834b61503cdda5b0331566a5cf59dc0d375a44afc4d10777",
     #     "path": "en_test_0/common_voice_en_27710027.mp3",
@@ -279,10 +285,253 @@ def load_commonvoice_classification(
     #     "segment": "",
     #     "variant": "",
     # }
-
+    language_list = [
+        "ab",
+        "af",
+        "am",
+        "ar",
+        "as",
+        "ast",
+        "az",
+        "ba",
+        "bas",
+        "be",
+        "bg",
+        "bn",
+        "br",
+        "ca",
+        "ckb",
+        "cnh",
+        "cs",
+        "cv",
+        "cy",
+        "da",
+        "de",
+        "dv",
+        "dyu",
+        "el",
+        "en",
+        "eo",
+        "es",
+        "et",
+        "eu",
+        "fa",
+        "fi",
+        "fr",
+        "fy-NL",
+        "ga-IE",
+        "gl",
+        "gn",
+        "ha",
+        "he",
+        "hi",
+        "hsb",
+        "ht",
+        "hu",
+        "hy-AM",
+        "ia",
+        "id",
+        "ig",
+        "is",
+        "it",
+        "ja",
+        "ka",
+        "kab",
+        "kk",
+        "kmr",
+        "ko",
+        "ky",
+        "lg",
+        "lij",
+        "lo",
+        "lt",
+        "ltg",
+        "lv",
+        "mdf",
+        "mhr",
+        "mk",
+        "ml",
+        "mn",
+        "mr",
+        "mrj",
+        "mt",
+        "myv",
+        "nan-tw",
+        "ne-NP",
+        "nhi",
+        "nl",
+        "nn-NO",
+        "nso",
+        "oc",
+        "or",
+        "os",
+        "pa-IN",
+        "pl",
+        "ps",
+        "pt",
+        "quy",
+        "rm-sursilv",
+        "rm-vallader",
+        "ro",
+        "ru",
+        "rw",
+        "sah",
+        "sat",
+        "sc",
+        "sk",
+        "skr",
+        "sl",
+        "sq",
+        "sr",
+        "sv-SE",
+        "sw",
+        "ta",
+        "te",
+        "th",
+        "ti",
+        "tig",
+        "tk",
+        "tok",
+        "tr",
+        "tt",
+        "tw",
+        "ug",
+        "uk",
+        "ur",
+        "uz",
+        "vi",
+        "vot",
+        "yi",
+        "yo",
+        "yue",
+        "zgh",
+        "zh-CN",
+        "zh-HK",
+        "zh-TW",
+        "zu",
+        "zza",
+    ]
     # the name of x and y
     x_label, y_label = "audio", "sentence"
-    ds = load_dataset(dataset_name, "en", split="test", streaming=True)
+    ds = load_dataset(dataset_name, language, split="test", streaming=True)
+    # filter
+    ds = ds.filter(lambda example: example[y_label])
+    return x_label, y_label, ds
+
+
+def load_google_fleurs_speaker_identify(language, dataset_name="google/fleurs"):
+    # https://huggingface.co/datasets/google/fleurs
+    # the name of x and y
+    language_list = [
+        "af_za",
+        "am_et",
+        "ar_eg",
+        "as_in",
+        "ast_es",
+        "az_az",
+        "be_by",
+        "bg_bg",
+        "bn_in",
+        "bs_ba",
+        "ca_es",
+        "ceb_ph",
+        "ckb_iq",
+        "cmn_hans_cn",
+        "cs_cz",
+        "cy_gb",
+        "da_dk",
+        "de_de",
+        "el_gr",
+        "en_us",
+        "es_419",
+        "et_ee",
+        "fa_ir",
+        "ff_sn",
+        "fi_fi",
+        "fil_ph",
+        "fr_fr",
+        "ga_ie",
+        "gl_es",
+        "gu_in",
+        "ha_ng",
+        "he_il",
+        "hi_in",
+        "hr_hr",
+        "hu_hu",
+        "hy_am",
+        "id_id",
+        "ig_ng",
+        "is_is",
+        "it_it",
+        "ja_jp",
+        "jv_id",
+        "ka_ge",
+        "kam_ke",
+        "kea_cv",
+        "kk_kz",
+        "km_kh",
+        "kn_in",
+        "ko_kr",
+        "ky_kg",
+        "lb_lu",
+        "lg_ug",
+        "ln_cd",
+        "lo_la",
+        "lt_lt",
+        "luo_ke",
+        "lv_lv",
+        "mi_nz",
+        "mk_mk",
+        "ml_in",
+        "mn_mn",
+        "mr_in",
+        "ms_my",
+        "mt_mt",
+        "my_mm",
+        "nb_no",
+        "ne_np",
+        "nl_nl",
+        "nso_za",
+        "ny_mw",
+        "oc_fr",
+        "om_et",
+        "or_in",
+        "pa_in",
+        "pl_pl",
+        "ps_af",
+        "pt_br",
+        "ro_ro",
+        "ru_ru",
+        "sd_in",
+        "sk_sk",
+        "sl_si",
+        "sn_zw",
+        "so_so",
+        "sr_rs",
+        "sv_se",
+        "sw_ke",
+        "ta_in",
+        "te_in",
+        "tg_tj",
+        "th_th",
+        "tr_tr",
+        "uk_ua",
+        "umb_ao",
+        "ur_pk",
+        "uz_uz",
+        "vi_vn",
+        "wo_sn",
+        "xh_za",
+        "yo_ng",
+        "yue_hant_hk",
+        "zu_za",
+        "all",
+    ]
+    x_label, y_label = "audio", "gender"
+    # load the right partition
+    ds = load_dataset(
+        dataset_name, language, split="test", streaming=True
+    )  # no test partition
     # filter
     ds = ds.filter(lambda example: example[y_label])
     return x_label, y_label, ds
@@ -300,5 +549,10 @@ def load_commonvoice_classification(
 #     return x_label, y_label, ds
 
 if __name__ == "__main__":
-    # x_label, y_label, ds = load_SLURP()
-    x_label, y_label, ds = load_IEMOCAP_emotion_recognition()
+    x_label, y_label, ds = load_via_eval(
+        dataset_name="CommonVoice_speaker_identity",
+        langauge="af_za",
+    )
+    import pdb
+
+    pdb.set_trace()
